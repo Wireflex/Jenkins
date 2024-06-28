48c24847813a962d257eb92c56a4b20cbc55eaff
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-06-29 02:28:31 +0500

    Update lesson1.tf

--- Terraform/lesson1.tf
+++ Terraform/lesson1.tf
@@ -1,1 +1,10 @@
+provider "aws" {}
 
+resource "aws_instance" "my_ubuntu" {
+  ami           = "ami-01e444924a2233b07"
+  instance_type = "t2.micro"
+}
+
+# export AWS_ACCESS_KEY_ID=ключ
+# export AWS_SECRET_ACCESS_KEY=ключ
+# export AWS_DEFAULT_REGION=регион
