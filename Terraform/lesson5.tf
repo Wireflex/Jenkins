3c737aeb043eaabfddd1be9a81a4a8a853ece472
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-06-29 23:47:21 +0500

    Create lesson5.tf

---
+++ Terraform/lesson5.tf
@@ -0,0 +1,9 @@
+  dynamic "ingress" {
+    for_each = ["80", "443", "8080", "1541", "9092", "9093"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
