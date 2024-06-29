ca44a6aacbcfb8676090ce9df26433491ffb88fb
Wireflex <165675775+Wireflex@users.noreply.github.com>
2024-06-30 00:42:33 +0500

    Update lesson6.tf

--- Terraform/lesson6.tf
+++ Terraform/lesson6.tf
@@ -1,3 +1,4 @@
+#==============================================#
   lifecycle {
     prevent_destroy = true
   }
@@ -12,4 +13,60 @@
 
 resource "aws_eip" "my_static_ip" {
   instance = aws_instance.allow_http_https_ssh.id
+}
+#=============================================#
+
+# TERRAFORM by Wireflex
+
+provider "aws" {}
+
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.web_server.id
+}
+resource "aws_security_group" "web_server" {
+  name = "allow_http_https_ssh"
+  description = "Allow HTTP, HTTPS and SSH inbound traffic"
+
+  dynamic "ingress" {
+    for_each = ["22", "80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0a3041ff14fb6e2be"
+  instance_type = "t2.micro"
+  key_name = "wireflex-key-frankfurt"
+
+  vpc_security_group_ids = [aws_security_group.web_serverh.id]
+
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Nikita",
+    l_name = "Utyaganov",
+    names  = ["Vasya", "Kolya", "Petya", "John", "Donald", "Masha"]
+  })
+
+  tags = {
+    Name  = "Web Server Build by Terraform"
+    Owner = "Wireflex"
+  }
+    lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web_server.public_ip
 }
